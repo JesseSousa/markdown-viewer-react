8e11be12edeef44a1f268675b9d0319349730939
Jesse Sousa <rhadamesjesse14rj@gmail.com>
2020-08-22 18:12:13 -0300

    Updates

---
+++ precache-manifest.cf86ce802ddb47adab4dfefad14f9bee.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1f908e3caac09922a5efcc87fd371323",
+    "url": "/markdown-previewer-react/index.html"
+  },
+  {
+    "revision": "af9059aa8e2628bc8afd",
+    "url": "/markdown-previewer-react/static/css/main.03dc1569.chunk.css"
+  },
+  {
+    "revision": "072fe0a363e736d4f492",
+    "url": "/markdown-previewer-react/static/js/2.20e8129d.chunk.js"
+  },
+  {
+    "revision": "40046c021c0025c71b5ec67323b92a85",
+    "url": "/markdown-previewer-react/static/js/2.20e8129d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "af9059aa8e2628bc8afd",
+    "url": "/markdown-previewer-react/static/js/main.07f81cbd.chunk.js"
+  },
+  {
+    "revision": "adc4c9422d9cc44db1c2",
+    "url": "/markdown-previewer-react/static/js/runtime-main.edfbe115.js"
+  }
+]);
